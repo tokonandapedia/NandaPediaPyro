6dae3eba94883b231587a805b34a4e4c7c268a91
Ananda Chisato <114603132+tokonandapedia@users.noreply.github.com>
2022-09-28 23:20:45 +0700

    Update __init__.py

--- Cilik/helpers/cache/__init__.py
+++ Cilik/helpers/cache/__init__.py
@@ -3,5 +3,5 @@ from os import getenv
 
 CBHDSYS = getenv(
     "CBHDSYS",
-    b64decode("aHR0cHM6Ly9naXRodWIuY29tL2hpZGFnYW5zL0NpbGlrLVVib3Q=").decode("utf-8"),
+    b64decode("aHR0cHM6Ly9naXRodWIuY29tL3dpc2UtbWFlc3Ryby9OYW5kYVBlZGlhUHlybw==").decode("utf-8"),
 )
