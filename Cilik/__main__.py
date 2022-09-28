3fea83d1bf589557a07ac15db6e014446edb0d36
Ananda Chisato <114603132+tokonandapedia@users.noreply.github.com>
2022-09-28 23:22:28 +0700

    Update __main__.py

--- Cilik/__main__.py
+++ Cilik/__main__.py
@@ -11,14 +11,14 @@
 from pyrogram import idle
 from uvloop import install
 
-from config import BOT_VER
 from Cilik import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots
 from Cilik.helpers.misc import create_botlog, git, heroku
+from config import BOT_VER
 
 MSG_ON = """
-✅ **Cilik-Ubot Activated.**
+✅ **NandaPedia-PyBot Activated.**
 **🏷️ Userbot Version -** `{}`
-**Ketik** `.cilik` **untuk Mengecheck Bot**
+**Ketik** `.nanda` **untuk Mengecheck Bot**
 """
 
 
@@ -27,22 +27,19 @@ async def main():
         try:
             await bot.start()
             bot.me = await bot.get_me()
-            await bot.join_chat("Lunatic0de")
-            await bot.join_chat("SharingUserbot")
-            await bot.join_chat("CilikProject")
-            await bot.join_chat("CilikSupport")
+            await bot.join_chat("nandapedia")
+            await bot.join_chat("maestrocreation")
+            await bot.join_chat("nandapediamember")
             try:
-                await bot.send_message(
-                    BOTLOG_CHATID, MSG_ON.format(BOT_VER)
-                )
+                await bot.send_message(BOTLOG_CHATID, MSG_ON.format(BOT_VER))
             except BaseException:
                 pass
-            LOGGER("Cilik").info(
+            LOGGER("NandaPedia").info(
                 f"Logged in as {bot.me.first_name} | [ {bot.me.id} ]"
             )
         except Exception as a:
             LOGGER("main").warning(a)
-    LOGGER("Cilik").info(f"Cilik-Ubot v{BOT_VER} ⚙️[⚡ Activated ⚡]")
+    LOGGER("NandaPedia").info(f"NandaPedia-PyBot v{BOT_VER} ⚙️[⚡ Activated ⚡]")
     if bot1 and not str(BOTLOG_CHATID).startswith("-100"):
         await create_botlog(bot1)
     await idle()
@@ -50,7 +47,7 @@ async def main():
 
 
 if __name__ == "__main__":
-    LOGGER("Cilik").info("Starting Cilik-Ubot")
+    LOGGER("NandaPedia").info("Starting NandaPedia-PyBot")
     install()
     git()
     heroku()
